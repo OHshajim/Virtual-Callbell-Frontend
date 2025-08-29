1c2f4f9c559f2f5cdfe77ab178d2f9c1c64e5f9d
OHshajim <ajshajimmax@gmail.com>
2025-08-29 14:48:52 +0600

    nav  updated

--- src/components/Dashboard/UserDashboardNavbar.jsx
+++ src/components/Dashboard/UserDashboardNavbar.jsx
@@ -14,55 +14,44 @@ const UserDashboardNavLinks = () => {
   const handleBack = () => {
     navigate(-1);
   };
-
-  const ulLinks = (
-    <>
-      <li
-        className={`hover:bg-indigo-600 ${
-          pathname === "/dashboard" ? "bg-indigo-600" : ""
-        } px-3 py-2 rounded-lg cursor-pointer transition `}>
-        <Link to={"/dashboard"}>Dashboard</Link>
-      </li>
-      <li
-        className={`hover:bg-indigo-600 ${
-          pathname === "/" ? "bg-indigo-600" : ""
-        } px-3 py-2 rounded-lg cursor-pointer transition `}>
-        <Link to={"/"}>Home</Link>
-      </li>
-      <li
-        className={`hover:bg-indigo-600 ${
-          pathname === "/dashboard/profile" ? "bg-indigo-600" : ""
-        } px-3 py-2 rounded-lg cursor-pointer transition `}>
-        <Link to={"/dashboard/profile"}>Profile</Link>
-      </li>
-      <li
-        className={`hover:bg-indigo-600 ${
-          pathname === "/dashboard/subscriptions" ? "bg-indigo-600" : ""
-        } px-3 py-2 rounded-lg cursor-pointer transition `}>
-        <Link to={"/dashboard/subscriptions"}>Subscriptions</Link>
-      </li>
-      <li
-        className={`hover:bg-indigo-600 ${
-          pathname === "/dashboard/transactions" ? "bg-indigo-600" : ""
-        } px-3 py-2 rounded-lg cursor-pointer transition `}>
-        <Link to={"/dashboard/transactions"}>Transaction</Link>
-      </li>
-      <li
-        onClick={handleLogout}
-        className={`hover:bg-indigo-600  px-3 py-2 rounded-lg cursor-pointer transition`}>
-        Logout
-      </li>
-    </>
-  );
+  const links = [
+    { href: "/dashboard", label: "Dashboard" },
+    { href: "/", label: "Home" },
+    { href: "/dashboard/profile", label: "Profile" },
+    { href: "/dashboard/subscriptions", label: "Subscriptions" },
+    { href: "/dashboard/transactions", label: "Transaction" },
+  ]
 
   return (
-    <>
-      <div className="flex flex-row justify-between items-center mb-8">
-        <ArrowLeft className="cursor-pointer border" onClick={handleBack} />
-        <h1 className="text-2xl font-bold">CallBell</h1>
-      </div>
-      <ul className="space-y-1">{ulLinks}</ul>
-    </>
+      <>
+          <div className="flex flex-row justify-between items-center mb-8">
+              <ArrowLeft
+                  className="cursor-pointer border"
+                  onClick={handleBack}
+              />
+              <h1 className="text-2xl font-bold">CallBell</h1>
+          </div>
+          <ul className="space-y-2 w-full flex flex-col">
+              {" "}
+              {links.map((l) => (
+                <Link to={l.href} key={l.href} >
+                  <li
+                    className={`hover:bg-indigo-600 ${
+                        pathname === l.href ? "bg-indigo-600" : ""
+                    } px-3 py-2 w-full rounded-lg cursor-pointer transition `}
+                  >
+                    {l.label}
+                  </li>
+                </Link>
+              ))}
+              <li
+                  onClick={handleLogout}
+                  className={`hover:bg-indigo-600  px-3 py-2 rounded-lg cursor-pointer transition`}
+              >
+                  Logout
+              </li>
+          </ul>
+      </>
   );
 };
 
